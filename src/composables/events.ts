bdac1c143642b3e6d26dc987058f5820b5fcd51a
DRACOX2500 <alexis.forestier2001@gmail.com>
2022-11-06 11:04:09 +0100

    get seen notif

--- src/composables/events.ts
+++ src/composables/events.ts
@@ -65,7 +65,7 @@ export function listenHighLevelClientEvents() {
 	chatClient.on<ConversationSeenEvent>(
 		'@conversationSeen',
 		async ({ conversation }) => {
-			//TODO
+			messengerStore.upsertConversation(conversation)
 		}
 	)
 
